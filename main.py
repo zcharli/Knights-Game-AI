ffcd2de941ff8de84081713a480f45b91de56551
Charlie Li <ok>
2016-01-15 10:28:26 -0500

    Can't start grid up without play game

--- main.py
+++ main.py
@@ -13,26 +13,24 @@ class GameBoard(wx.Frame):
     def __init__(self, title):
         wx.Frame.__init__(self, None, title=title, pos=(150, 150), size=(800, 800))
         self.__do_layout()
+        self._register_handlers()
+        self._play_game(self.txtDimensions.GetValue())
 
     def __do_layout(self):
         sizer = wx.FlexGridSizer(cols=6, hgap=6, vgap=6)
-        self.btnPlayGame = wx.Button(self, PLAYGAME, "Restart Game")
+        self.btnPlayGame = wx.Button(self, PLAYGAME, "Play Game")
         self.btnDFS = wx.Button(self, DFS, "Depth First Search")
         self.btnBFS = wx.Button(self, DFS, "Breadth First Search")
         self.btnAStar = wx.Button(self, DFS, "A* Search")
         lblDimensions = wx.StaticText(self, -1, "Grid Dimensions")
-        self.txtDimensions = wx.TextCtrl(self, -1, "30", size=(125, -1))
-
+        self.txtDimensions = wx.TextCtrl(self, -1, "20", size=(125, -1))
         sizer.AddMany([self.btnPlayGame, lblDimensions, self.txtDimensions,
                        self.btnBFS, self.btnDFS, self.btnAStar])
-
         self.screen = wx.BoxSizer(wx.VERTICAL)
         self.screen.Add(sizer, 0, wx.ALL, 25)
         self.board = None
         self.SetSizer(self.screen)
         self.SetAutoLayout(True)
-        self._register_handlers()
-        self._play_game(self.txtDimensions.GetValue())
 
     def _register_handlers(self):
         wx.EVT_BUTTON(self, PLAYGAME, self._play_game)
@@ -40,25 +38,23 @@ class GameBoard(wx.Frame):
     def _play_game(self, event):
         if self.board:
             print "Removing Board"
-            self._remove_board();
+            self._remove_board()
         self.board = self._build_board_canvas(int(self.txtDimensions.GetValue()))
         self.screen.Add(self.board)
-        self.Refresh()
+        self.screen.Layout()
+
 
     def _remove_board(self):
         self.board.Clear()
-        self.boardCanvas.ClearAll()
-        self.screen.Hide(self.board)
-        self.screen.Remove(self.board)
         self.board = None
 
     def _build_board_canvas(self, dimension):
         box = wx.BoxSizer(wx.VERTICAL)
         board_canvas = FloatCanvas.FloatCanvas(self, size=(800, 700),
-                                              ProjectionFun=None,
-                                              Debug=0,
-                                              BackgroundColor="Black",
-                                              )
+                                               ProjectionFun=None,
+                                               Debug=0,
+                                               BackgroundColor="Black",
+                                               )
         self.boardCanvas = board_canvas
         self.boardCanvas.Bind(wx.EVT_SIZE, self._on_size)
         w = 30
@@ -68,7 +64,6 @@ class GameBoard(wx.Frame):
                 landable_square = self.boardCanvas.AddRectangle((i * dx, j * dx), (w, w),
                                                                 FillColor="White", LineStyle=None)
                 landable_square.indexes = (i, j)
-
         box.Add(board_canvas)
         return box
 
@@ -94,8 +89,8 @@ class KnightsBoard(wx.Panel):
         self.boardCanvas = boardCanvas
         self.boardCanvas.Bind(wx.EVT_SIZE, self._on_size)
         # build the squares:
-        w = 2
-        dx = 4
+        w = 30
+        dx = 32
         for i in range(9):
             for j in range(9):
                 landableSquare = self.boardCanvas.AddRectangle((i * dx, j * dx), (w, w),
